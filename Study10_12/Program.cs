0123e8423de4cf885c738d62a1fb08cd1759ec21
Kwon <kkynks0@naver.com>
2022-07-06 21:59:55 +0900

    C# 연산자

--- Study10_12/Program.cs
+++ Study10_12/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using System;
+using static System.Console;
+
+
+/*
+class LogicalAnd
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"true && true -> {true && true} ");
+        Console.WriteLine($"true && false -> {true && false} ");
+        Console.WriteLine($"false && false -> {false && false} ");
+    }
+}
+*/
+
+/*
+class LogicalOr
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"true || true -> {true || true} ");
+        Console.WriteLine($"true || false -> {true || false} ");
+        Console.WriteLine($"false || false -> {false || false} ");
+    }
+}
+*/
+
+/*
+class LogicalNot
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"!true -> {!true} ");
+        Console.WriteLine($"!false -> {!false} ");
+    }
+}
+*/
+
+/*
+class BitwiseOperator
+{
+    static void Main(string[] args)
+    {
+        byte x = 0b1010;
+        byte y = 0b1100;
+        WriteLine($" {Convert.ToString(x,2)} -> {x}");
+        WriteLine($"| {Convert.ToString(y, 2)} -> {y}");
+
+
+        WriteLine($"{Convert.ToString(x | y, 2)} -> {x | y}");
+    }
+}
+*/
+
